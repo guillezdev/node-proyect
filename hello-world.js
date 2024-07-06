a3c9efaa213ead326161a64a798005926d522975
Guillermo González Calderín <guillezdev@gmail.com>
2024-07-05 20:54:13 -0400

    get os info and change to ESM

--- hello-world.js
+++ hello-world.js
@@ -1,9 +1,9 @@
-const http = require('node:http');
-
+import { createServer } from 'node:http';
+import os from 'node:os';
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
+const server = createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello, World!\n');
@@ -11,4 +11,14 @@ const server = http.createServer((req, res) => {
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
+  console.log('Plataforma:', os.platform());
+  console.log('Arquitectura:', os.arch());
+  console.log('Versión del SO:', os.version());
+  console.log('Memoria total (bytes):', os.totalmem());
+  console.log('Memoria libre (bytes):', os.freemem());
+  console.log('Directorio temporal:', os.tmpdir());
+  console.log('Nombre del host:', os.hostname());
+  console.log('CPU:', os.cpus());
+  console.log('Información de usuario actual:', os.userInfo());
+  console.log('Directorio de inicio del usuario actual:', os.homedir());
 }); 
