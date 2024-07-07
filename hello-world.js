62a15b123ebe65f75bfc808f90e6cf27cae2c05d
Guillermo González Calderín <guillezdev@gmail.com>
2024-07-06 22:57:00 -0400

    update

--- hello-world.js
+++ hello-world.js
@@ -3,7 +3,7 @@ import os from 'node:os';
 import pc from "picocolors"
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 0;
 
 const server = createServer((req, res) => {
   res.statusCode = 200;
